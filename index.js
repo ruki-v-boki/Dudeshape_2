c36038cb167c9ebd870235da59d8e9c1a60872bc
ruki_v_boki <maksiimivanoov@yandex.ru>
2025-04-15 10:13:42 +0300

    больше анимации!

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ likeButtons.forEach(btn => {
 
 
 // ------------ СЛАЙДЕР ------------
-function initSlider() {
-  const slider = document.querySelector('.slider')
-  const container = slider.querySelector('.slider__slides-container')
+const slider = document.querySelector('.slider')
+const container = slider.querySelector('.slider__slides-container')
+
+function initSlider(slider) {
   const slides = slider.querySelectorAll('.slides-list__item')
   const slideWidth = slides[0].offsetWidth
   const prevBtn = slider.querySelector('.slider__button-nav--prev')
@@ -37,21 +38,108 @@ function initSlider() {
   checkButtons()
 }
 
-initSlider();
+initSlider(slider);
 
 
 // ------------ АНИМАЦИЯ ------------
-const animatedBlocks = document.querySelectorAll('.scroll-animate')
+const animatedBlocks = document.querySelectorAll('.scroll-animate');
+const imageBox = document.querySelector('.furniture__content-image-box');
+const reviewImage = document.querySelector('.reviews__image-box')
+const reviewText = document.querySelector('.reviews__text')
+const aboutUsImage = document.querySelector('.about-us__image')
 
+
+// Появление
 animatedBlocks.forEach(block => {
   const observer = new IntersectionObserver(entries => {
-    const entry = entries[0] // Первая запись в массиве
-    if (entry.isIntersecting) {
-      entry.target.classList.add('scroll-animate-visible')
-    }
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('scroll-animate-visible');
+        observer.unobserve(entry.target)
+      }
+    })
   }, {
-    threshold: 0.3 // 30% видимости
+    threshold: 0.5 // % видимости
   })
 
   observer.observe(block)
+});
+
+//Рамки Картинки
+const imageBordersObserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate-borders')
+      imageBordersObserver.unobserve(entry.target)
+    }
+  })
+}, {
+  threshold: 0.5
+  })
+
+imageBordersObserver.observe(imageBox)
+
+//Кружок
+const circleObserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate-circle')
+      circleObserver.unobserve(entry.target)
+    }
+  })
+}, {
+  threshold: 0.5
+  })
+
+circleObserver.observe(reviewImage)
+
+//О нас
+const aboutUsObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate-image')
+      observer.unobserve(entry.target)
+    }
+  })
+}, {
+  threshold: 0.5,
 })
+
+aboutUsObserver.observe(aboutUsImage)
+
+//Слайдер
+const sliderObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate-slider')
+      observer.unobserve(entry.target)
+    }
+  })
+}, {
+  threshold: 0.5,
+})
+
+sliderObserver.observe(container)
+
+//Текст
+
+const textObserver = new IntersectionObserver(entries => {
+  if (entries[0].isIntersecting) {
+    const text = reviewText.textContent
+    reviewText.textContent = ''
+
+    new Typed(reviewText, {
+      strings: [text],
+      typeSpeed: 40,
+      backSpeed: 0,
+      showCursor: false,
+      loop: false
+    })
+
+    textObserver.unobserve(reviewText)
+  }
+  }, {
+    threshold: 0.3
+  })
+
+textObserver.observe(reviewText)
